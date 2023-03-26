2cf80d5dbd0403fca2e7d1b2ce454364eddea356
Lilian Bittar <bittarlily@gmail.com>
2023-03-26 19:57:35 +0200

    Understanding the component.

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
+    // selector: '[app-server]',
+    // selector: '.app-servers',
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
