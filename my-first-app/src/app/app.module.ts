3d38bece29c6561a3163b56be8271db7bad1bd04
Lilian Bittar <60697037+LilianBittar@users.noreply.github.com>
2023-07-13 00:30:41 +0200

    Created a core module.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -30,15 +28,10 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    CoreModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([])
   ],
-  providers: [ShoppingListService, 
-              RecipeService, 
-              {
-                provide: HTTP_INTERCEPTORS, 
-                useClass: AuthInterceptorService, 
-                multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
