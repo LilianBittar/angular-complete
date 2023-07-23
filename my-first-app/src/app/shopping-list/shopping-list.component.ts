0b902e5b20ac121b1953cbaf0c2017af1d6107f9
Lilian Bittar <60697037+LilianBittar@users.noreply.github.com>
2023-07-23 13:42:46 +0200

    Selecting state.

--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,27 +11,29 @@ import { LoggingService } from '../logging.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy{
-  ingredients: Ingredient[];
+  ingredients: Observable< { ingredients: Ingredient[] } >; 
   private igChangedSub: Subscription;
 
   constructor (private slService: ShoppingListService, 
-              private loggingService: LoggingService) { }
+              private loggingService: LoggingService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[] } }>) { }
 
   ngOnInit() {
-      this.ingredients = this.slService.getIngredients();
-      this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
-      this.igChangedSub = this.slService.ingredientsChanged.
-      subscribe(
-        (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      })
+      this.ingredients = this.store.select('shoppingList');
+      // this.ingredients = this.slService.getIngredients();
+      // this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
+      // this.igChangedSub = this.slService.ingredientsChanged.
+      // subscribe(
+      //   (ingredients: Ingredient[]) => {
+      //   this.ingredients = ingredients;
+      // })
   }
 
   onEditItem(index: number){
     this.slService.startedEditing.next(index);
   }
   ngOnDestroy(){
-    this.igChangedSub.unsubscribe();
+    // this.igChangedSub.unsubscribe();
   }
  
 }
