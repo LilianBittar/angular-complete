329f1b04772bc31191bd6eb896b6bb14af0aa9df
Lilian Bittar <60697037+LilianBittar@users.noreply.github.com>
2023-07-23 23:52:20 +0200

    Removed the shopping list service.

--- my-first-app/src/app/recipes/recipe.service.ts
+++ my-first-app/src/app/recipes/recipe.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Subject } from "rxjs";
 import { Store } from "@ngrx/store";
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.action';
@@ -30,7 +29,7 @@ export class RecipeService{
       //];
   private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService, 
+  constructor(
     private store: Store<fromShoppingList.AppState>) 
   { }
 
--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import { Observable, Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
 import { Store } from '@ngrx/store';
@@ -16,7 +15,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy{
   ingredients: Observable< { ingredients: Ingredient[] } >; 
   private igChangedSub: Subscription;
 
-  constructor (private slService: ShoppingListService, 
+  constructor (
               private loggingService: LoggingService,
               private store: Store<fromShoppingList.AppState>) { }
 
